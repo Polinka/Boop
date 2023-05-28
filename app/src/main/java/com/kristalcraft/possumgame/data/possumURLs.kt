5cf712653a9f3669ef650af41380390328f4d8fe
Kristalcraft <dmbezoar@gmail.com>
2023-05-28 14:35:22 +0300

    Initial commit

---
+++ app/src/main/java/com/kristalcraft/possumgame/data/possumURLs.kt
@@ -0,0 +1,19 @@
+package com.kristalcraft.possumgame.data
+
+val possumImages = arrayListOf("https://pbs.twimg.com/media/FxK460-WYAYaUBN?format=jpg&name=small",
+"https://pbs.twimg.com/media/FxKrL1FWwAE1REV?format=jpg&name=small",
+"https://pbs.twimg.com/media/FxKB_FyXwAEu5BF?format=jpg&name=900x900",
+"https://pbs.twimg.com/media/FxJ0QODXsAIs15k?format=jpg&name=small",
+"https://pbs.twimg.com/media/FxJmhREXwAIlKZa?format=jpg&name=small",
+"https://pbs.twimg.com/media/FxGKyxTWcAMUTJA?format=jpg&name=small",
+"https://pbs.twimg.com/media/FxF9D3QXsAEL9d0?format=jpg&name=900x900",
+"https://pbs.twimg.com/media/FxEc7knWAAEisDb?format=jpg&name=900x900",
+"https://pbs.twimg.com/media/FxEBdurWcAEgj56?format=jpg&name=small",
+"https://pbs.twimg.com/media/FxCF3jkWwAMKIVz?format=jpg&name=900x900",
+"https://pbs.twimg.com/media/FxBBNBaWwAIgTsQ?format=jpg&name=small",
+"https://pbs.twimg.com/media/FxAKRfQWIAIwhf6?format=jpg&name=small",
+"https://pbs.twimg.com/media/Fw-caJhXsAAYjUj?format=jpg&name=small",
+"https://pbs.twimg.com/media/Fw8FWMIXoAIBEmn?format=jpg&name=small",
+"https://pbs.twimg.com/media/Fw7cJdLX0AABkrG?format=jpg&name=small",
+"https://pbs.twimg.com/media/Fw7ArqvWAAEuY1p?format=jpg&name=small",
+"https://files.botsin.space/media_attachments/files/110/425/199/024/608/617/original/41e3d512ca7d1705.jpg")
